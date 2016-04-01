548d0f9b51db8e8c965e84e9fa4d300e0572d765
womacks <samuel.womack@wwt.com>
2016-04-01 14:23:46 -0500

    Streamlined with function to generate path for version..and wrote version 8 paths

--- index.js
+++ index.js
@@ -1,40 +1,49 @@
 var fs = require('fs');
 
 var prgFiles = 'C:\\Program Files\\Java\\',
-    bser = '\\bin\\server\\;'
-    j7 = '1.7.0',
-    j8 = '1.8.0',
-    jdk7 = `jdk${j7}`,
-    jre7 = `jre${j7}`,
-    jdk8 = `jdk${j8}`,
-    jre8 = `jre${j8}`;
+    binSer = '\\bin\\server\\;';
+
+var baseJavaVersion = (jver, update) => {
+  var ver = `1.${jver}.0`,
+      jdk = `jdk${ver}${(update) ? ('_' + update) : ''}`,
+      jre = `jre${ver}${(update) ? ('_' + update) : ''}`;
+  return (
+    `${prgFiles + jdk + '\\jre' + binSer}`+
+    `${prgFiles + jre + binSer}`
+  );
+};
+
 var pathStr = '';
 var i;
 for(i=0; i<81; i++) {
   if(i===0) {
-    // console.log(`${prgFiles + jdk7}`);
-    // console.log(`${prgFiles + jre7}`);
-    pathStr+=`${prgFiles+jdk7+'\\jre'+bser}`;
-    pathStr+=`${prgFiles+jre7+bser}`;
+    pathStr+=';' + baseJavaVersion('7');
   } else {
-    var iter = i.toString();
-    var rel;
-    rel = ((i>=1 && i<=7) || (i>=9 && i<=11) ||
+    var rel7 = ((i>=1 && i<=7) || (i>=9 && i<=11) ||
       i===13 || i===15 || i===17 || i===21 || i===25 ||
       i===40 || i===45 || i===51 || i===55 || i===60 ||
       i===65 || i===67 || i===71 || i===76 || i===79 ||
       i===80)
-    ? iter
-    : null;
-    if(rel) {
-      // console.log(`${prgFiles + jdk7 + '_' + i}`);
-      // console.log(`${prgFiles + jre7 + '_' + i}`);
-      pathStr+=`${prgFiles + jdk7 + '_' + i + '\\jre' + bser}`;
-      pathStr+=`${prgFiles + jre7 + '_' + i + bser}`;
+    ? i.toString()
+    : undefined;
+    if(rel7) {
+      pathStr+= baseJavaVersion('7', rel7);
     }
-    console.log(pathStr);
   }
 }
 
 for(i=0; i<81; i++) {
+  if(i===0) {
+    pathStr+=baseJavaVersion('8');
+  } else {
+    var rel8 = (i===5 || i===11 || i===20 || i===25 ||
+      i===31 || i===40 || i===45 || i===51 || i===60 ||
+      (i>=65&&i<=66) || (i>=71&&i<=74))
+    ? i.toString()
+    : undefined
+    if(rel8) {
+      pathStr+= baseJavaVersion('8', rel8);
+    }
+  }
 }
+console.log(pathStr);
